b5f04cf809ed65ca3ed5552f7033bccba8bfd309
suryawan <rizalsuryawan86@gmail.com>
2023-06-27 16:12:54 +0700

    tipe data any

---
+++ 08-tipe-data-any/index.js
@@ -0,0 +1,6 @@
+// tipe data any pada typescript
+var myName;
+myName = "rocker";
+myName = 23;
+console.log(myName);
+// tipe data any pada typescript
---
+++ 08-tipe-data-any/index.ts
@@ -0,0 +1,10 @@
+// tipe data any pada typescript
+let myName: any;
+
+myName = "rocker";
+myName = 23;
+
+console.log(myName);
+// tipe data any pada typescript
+
+// tipe data any kurang baik digunakan dalam typescript karena menghilangkan citra typescript itu sendiri, karena dengan menggunakan tipe data any maka menghilangkan proses type checking yang merupakan ciri khas typescript itu sendiri.
