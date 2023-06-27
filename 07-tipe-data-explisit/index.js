272bb9f06ce06de0aa3bd2f7101537775a366808
suryawan <rizalsuryawan86@gmail.com>
2023-06-27 15:54:04 +0700

    tipe data ekplisit

--- 07-tipe-data-explisit/index.js
+++ 07-tipe-data-explisit/index.js
@@ -1,3 +1,11 @@
-// tipe data explisit pada typescript
-
-// tipe data explisit pada typescript
+// tipe data ekplisit pada typescript
+var myName = "Rizal";
+var myAge;
+myAge = 24;
+//console.log(myAge);
+// tipe data ekplisit pada typescript
+// tipe data array secara ekplisit
+var Campuran = [];
+Campuran.push(1, 2);
+console.log(Campuran);
+// tipe data array secara ekplisit
--- 07-tipe-data-explisit/index.ts
+++ 07-tipe-data-explisit/index.ts
@@ -1,3 +1,19 @@
-// tipe data explisit pada typescript
+// tipe data ekplisit pada typescript
 
-// tipe data explisit pada typescript
+let myName: string = "Rizal";
+
+let myAge: number;
+myAge = 24;
+
+//console.log(myAge);
+
+// tipe data ekplisit pada typescript
+
+// tipe data array secara ekplisit
+
+let Campuran: string[] = [];
+
+Campuran.push("Budiono", "Santoso");
+
+console.log(Campuran);
+// tipe data array secara ekplisit
